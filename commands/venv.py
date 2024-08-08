f0cecd6ec5d530a1b5ef44226f3808ae925f0bdd
Acutapugione <acuta.pugione@gmail.com>
2024-08-08 20:14:18 +0300

    ...

--- commands/venv.py
+++ commands/venv.py
@@ -4,6 +4,7 @@ from . import StrEnum
 class VenvCommands(StrEnum):
     DEACTIVATE = "deactivate"
     CREATE = "python3 -m venv .venv"
-    ACTIVATE = ". ./.venv/bin/activate"
+    ACTIVATE = ". ./.venv/bin/activate"  # if LINUX
+    # ACTIVATE = ".\\.venv\\Scripts\\Activate"  # if Daunindows
     INSTALL_REQUIREMENTS = "./.venv/bin/pip install -r requirements.txt"
     # RUN_MAIN = "./.venv/bin/python3 main.py"
