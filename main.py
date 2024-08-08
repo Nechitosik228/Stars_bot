f97d807ed1377dca4f054ca88e1c6bb742d03143
Acutapugione <acuta.pugione@gmail.com>
2024-08-08 19:55:00 +0300

    ...

--- main.py
+++ main.py
@@ -1,12 +1,16 @@
 from os import getenv
+
 from dotenv import load_dotenv
 from uvicorn import run
 from backend import app
 
 
+def run_backend():
+    run(app=app, host=getenv("API_HOST"), port=int(getenv("API_PORT")))
+
+
 if __name__ == "__main__":
+    load_dotenv()
+    run_backend()
     # API_HOST = localhost
     # API_PORT = 4141
-
-    load_dotenv()
-    run(app=app, host=getenv("API_HOST"), port=int(getenv("API_PORT")))
